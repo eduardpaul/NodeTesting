6cd859c98767f9ede08a1e5e3957e31e405acd72
Eduard Paul <eduardpaul1091+github@gmail.com>
2015-05-10 22:50:31 +0200

    Server <-> client interaction.

---
+++ 01.Example/client.js
@@ -0,0 +1,28 @@
+/// <reference path="../typings/node/node.d.ts"/>
+
+var http = require('http');
+
+var options = {
+  host: '127.0.0.1',
+  port: 1337,
+  path: '/',
+  method: 'GET'
+};
+
+var req = http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  
+  res.setEncoding('utf8');
+ 
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+ 
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+req.end();
